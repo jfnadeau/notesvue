0acf2aad3bd4a22f2d31f13b94ad1baff0e43b12
Nadeau <jnade10@AVDL.com>
2018-01-05 13:42:10 -0500

    more unit tests

--- src/components/note-editor/note-editor.test.ts
+++ src/components/note-editor/note-editor.test.ts
@@ -1,13 +1,53 @@
-import { shallow } from "vue-test-utils";
+import { shallow, Wrapper } from "vue-test-utils";
 import { NoteEditor } from "./note-editor";
+import { Note } from "../../services/notes/model";
+import Vue from "vue";
+import { createRenderer } from "vue-server-renderer";
 
 describe("NoteEditor", () => {
 
   it("should render like snapshot", () => {
 
     let sut = shallow(NoteEditor);
-    expect(sut.element).toMatchSnapshot();
+    expect(createRenderer().renderToString(sut.vm)).resolves.toMatchSnapshot();
+
+  });
+
+  it("should render note content when note prop is updated", async () => {
+
+    let newNote: Note = {
+      title: "new title",
+      description: "new description"
+    };
+
+    let sut = shallow(NoteEditor);
+    sut.setProps({ initialNote: newNote });
+
+    expect(createRenderer().renderToString(sut.vm)).resolves.toMatchSnapshot();
+  });
+
+  it("should emit save event when save button is clicked", () => {
+
+    let sut = shallow(NoteEditor);
+    setInputValue(sut, "input[name='title']", "Input title");
+    setInputValue(sut, "textarea[name='description']", "Input desc");
+
+    sut.find("button").trigger("click");
+
+    expect(sut.emitted("save")[0]).toEqual(
+      [{ title: "Input title", description: "Input desc" } as Note]
+    );
 
   });
 
 });
+
+function setInputValue<V extends Vue>(
+  wrapper: Wrapper<V>,
+  selector: string,
+  value: string) {
+
+  let inputWrapper = wrapper.find(selector);
+  (inputWrapper.element as HTMLInputElement).value = value;
+  inputWrapper.trigger("input");
+}
